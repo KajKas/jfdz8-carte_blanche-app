3d5e88198db712d83e5348a5c4502551ba591535
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-08-26 14:00:20 +0200

    playing with leaflet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import EventsMap from "./EventsMap";
+
 
 class App extends Component {
 
   state = {
-    events: []
+    events: [],
+
   }
 
   componentDidMount() {
@@ -32,15 +34,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+      <EventsMap/>
+
+
     )
   }
 }
