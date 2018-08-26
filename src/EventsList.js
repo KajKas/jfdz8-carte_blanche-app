053ac574f19666eb45d568ff0bf840228a78d58a
Ewa Siwek <ew.siwek@gmail.com>
2018-08-26 12:23:24 +0200

    Added EventsList component

---
+++ src/EventsList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class EventsList extends Component {
+    render() {
+        return (
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
+        );
+    }
+}
+
+export default EventsList;
