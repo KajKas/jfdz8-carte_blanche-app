a85fa50b9f9396257726773e27ecf38818210ff8
KajKas <kajetankasperuk@gmail.com>
2018-09-09 14:01:50 +0200

    map, markers and popups working

--- src/EventsMap.js
+++ src/EventsMap.js
@@ -12,19 +12,20 @@ L.Icon.Default.mergeOptions({
 
 class EventsMap extends Component {
 
-  position = [54.40, 18.57];
-
+  state = {
+    position: [54.40, 18.57]
+  }
 
   render() {
     return (
 
       <div >
-        <Map center={this.position} zoom={13}  style={{height: '400px', width:'400px'}}>
+        <Map center={this.state.position} zoom={13}  style={{height: '400px', width:'400px'}}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           />
-          <Marker position={this.position}>
+          <Marker position={this.state.position}>
             <Popup>A pretty CSS3 popup.<br/>Easily customizable.</Popup>
           </Marker>
         </Map>
