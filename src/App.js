99477a7adfb70fc352a4b465b46882149cbc23d9
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-09-16 19:03:32 +0200

    promise.all?

--- src/App.js
+++ src/App.js
@@ -34,17 +34,27 @@ class App extends Component {
       .then(response => response.json())
       .then(
         events => events.map(
-          event => fetch('https://isa-cors-proxy.herokuapp.com/api/rest/places.json?id=' + event.id)
+          event => fetch('https://isa-cors-proxy.herokuapp.com/api/rest/places.json?id=' + event.place.id)
             .then(response => response.json())
             .then(
-              // places => places.map(
+              places => places.map(
                 places => this.setState({
                   places: places
                 })
-              // )
+              )
             )
         )
       )
+  }
+
+  getPlaces = () => {
+    let eventsPromise = fetch('https://isa-cors-proxy.herokuapp.com/api/rest/events.json').then(response => { return response.json() });
+    let placesPromise = fetch('https://isa-cors-proxy.herokuapp.com/api/rest/places.json?id=' + event.place.id).then(response => { return response.json() });
+    Promise.all([eventsPromise, placesPromise]).then(response => {
+      return response[0].map(list => response[1].filter(item => item.listId === list.id))[0]
+    }).then(response => {
+      response.map(places => this.setState({places: places}))
+    })
   }
 
 
