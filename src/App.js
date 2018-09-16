4936c53c7cd607ba449e4dfbb6c9369186374d9d
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-09-16 18:07:16 +0200

    events and ALL the places in the state

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ import MainMap from "./EventsMap";
 class App extends Component {
 
   state = {
-    // events: [],
+    events: [],
     places: []
   }
 
   componentDidMount() {
-    // this.getEvents()
+    this.getEvents()
     this.getPlaces()
   }
 
-  // getEvents() {
-  //   fetch(
-  //     'https://isa-cors-proxy.herokuapp.com/api/rest/events.json'
-  //   )
-  //     .then(response => response.json())
-  //     .then(
-  //     data => this.setState({
-  //       events: data
-  //     })
-  //   )
-  // }
+  getEvents() {
+    fetch(
+      'https://isa-cors-proxy.herokuapp.com/api/rest/events.json'
+    )
+      .then(response => response.json())
+      .then(
+      data => this.setState({
+        events: data
+      })
+    )
+  }
 
   getPlaces() {
     fetch(
@@ -34,13 +34,16 @@ class App extends Component {
       .then(response => response.json())
       .then(
         events => events.map(
-          event => fetch('https://isa-cors-proxy.herokuapp.com/api/rest/places?id=' + event.id)
+          event => fetch('https://isa-cors-proxy.herokuapp.com/api/rest/places.json?id=' + event.id)
+            .then(response => response.json())
+            .then(
+              // places => places.map(
+                places => this.setState({
+                  places: places
+                })
+              // )
+            )
         )
-      )
-      .then(
-        places => this.setState({
-          places: places
-        })
       )
   }
 
