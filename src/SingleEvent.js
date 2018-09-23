d1df68c60d1a329e11d7c9cbc610f49673ad65fe
KajKas <kajetankasperuk@gmail.com>
2018-09-23 13:03:10 +0200

    reformatted code

--- src/SingleEvent.js
+++ src/SingleEvent.js
@@ -2,7 +2,6 @@ import React, {Component, Fragment} from 'react'
 
 class SingleEvent extends Component {
 
-
   formatDate(stringDate) {
     const date = new Date(stringDate)
 
@@ -30,29 +29,25 @@ class SingleEvent extends Component {
     return (
       this.props.event === undefined ?
         <p>Loading event data...</p> :
-      <Fragment>
-        <h1>{this.props.event.name}</h1>
-
-        {
-          this.props.event.attachments.map(image => (<img src={image.fileName}/>))
-        }
-
-        <p>Rozpoczęcie: {this.formatDate(this.props.event.startDate)}</p>
-        <p>Zakończenie: {this.formatDate(this.props.event.endDate)}</p>
-        <p>Organizator: {this.props.event.organizer.designation}</p>
-        <p>Miejsce wydarzenia: {this.props.event.place.name}</p>
-        <p>Ceny biletów:
+        <Fragment>
+          <h1>{this.props.event.name}</h1>
           {
-            this.props.event.tickets.startTicket ?
-            ' ' + this.props.event.tickets.startTicket + 'zł - ' + this.props.event.tickets.endTicket + 'zł' :
-            ' nieznane'
+            this.props.event.attachments.map(image => (<img src={image.fileName}/>))
           }
-        </p>
-        <p>Strona wydarzenia: <a href={this.props.event.urls.www}>{this.props.event.urls.www}</a></p>
-        <div dangerouslySetInnerHTML={{__html: this.props.event.descLong}} />
-
-
-      </Fragment>
+          <p>Rozpoczęcie: {this.formatDate(this.props.event.startDate)}</p>
+          <p>Zakończenie: {this.formatDate(this.props.event.endDate)}</p>
+          <p>Organizator: {this.props.event.organizer.designation}</p>
+          <p>Miejsce wydarzenia: {this.props.event.place.name}</p>
+          <p>Ceny biletów:
+            {
+              this.props.event.tickets.startTicket ?
+              ' ' + this.props.event.tickets.startTicket + 'zł - ' + this.props.event.tickets.endTicket + 'zł' :
+              ' nieznane'
+            }
+          </p>
+          <p>Strona wydarzenia: <a href={this.props.event.urls.www}>{this.props.event.urls.www}</a></p>
+          <div dangerouslySetInnerHTML={{__html: this.props.event.descLong}} />
+        </Fragment>
     )
   }
 }
