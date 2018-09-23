021c502f94d995896f9ab5ab7b9b2410b595e3be
KajKas <kajetankasperuk@gmail.com>
2018-09-23 12:54:17 +0200

    single event edited

--- src/SingleEvent.js
+++ src/SingleEvent.js
@@ -1,6 +1,30 @@
 import React, {Component, Fragment} from 'react'
 
 class SingleEvent extends Component {
+
+
+  formatDate(stringDate) {
+    const date = new Date(stringDate)
+
+    let year = date.getFullYear()
+    let month = date.getMonth()+1
+    let day = date.getDate()
+    let hour = date.getHours()
+    let minutes = date.getMinutes()
+
+    if (day < 10) {
+      day = '0' + day
+    }
+    if (month < 10) {
+      month = '0' + month
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes
+    }
+
+    return day + '-' + month + '-' + year + '  ' + hour + ':' + minutes
+  }
+
   render() {
 
     return (
@@ -13,8 +37,8 @@ class SingleEvent extends Component {
           this.props.event.attachments.map(image => (<img src={image.fileName}/>))
         }
 
-        <p>Rozpoczęcie: {this.props.event.startDate}</p>
-        <p>Zakończenie: {this.props.event.endDate}</p>
+        <p>Rozpoczęcie: {this.formatDate(this.props.event.startDate)}</p>
+        <p>Zakończenie: {this.formatDate(this.props.event.endDate)}</p>
         <p>Organizator: {this.props.event.organizer.designation}</p>
         <p>Miejsce wydarzenia: {this.props.event.place.name}</p>
         <p>Ceny biletów:
@@ -25,7 +49,7 @@ class SingleEvent extends Component {
           }
         </p>
         <p>Strona wydarzenia: <a href={this.props.event.urls.www}>{this.props.event.urls.www}</a></p>
-        <p>{this.props.event.descLong}</p>
+        <div dangerouslySetInnerHTML={{__html: this.props.event.descLong}} />
 
 
       </Fragment>
