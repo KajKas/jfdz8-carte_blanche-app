dfacd92109e52ecc6d4aafbd2e5e0b6a6eb05436
ewa1siwek <ew.siwek@gmail.com>
2018-09-23 12:42:29 +0200

    Added firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'leaflet/dist/leaflet.css'
 
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyB9Yh0qBZ7FYufkdsK7YbXUYD8UXbohtHU",
+    authDomain: "carte-blanche-1122e.firebaseapp.com",
+    databaseURL: "https://carte-blanche-1122e.firebaseio.com",
+    projectId: "carte-blanche-1122e",
+    storageBucket: "carte-blanche-1122e.appspot.com",
+    messagingSenderId: "718237421345"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
