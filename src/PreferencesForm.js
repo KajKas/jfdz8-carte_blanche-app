6d18f94dc0f41f9e21fa3fe75efd07af5ee0f2ab
KajKas <kajetankasperuk@gmail.com>
2018-09-24 21:39:01 +0200

    preferences with checkboxes and select options

--- src/PreferencesForm.js
+++ src/PreferencesForm.js
@@ -27,96 +27,116 @@ class PreferencesForm extends Component {
   render() {
     return (
       <Fragment>
-        <h3>Kino</h3>
-        <select name="prferences">
-          {
-            this.state.categories.filter(categories => categories.id > 99).filter(categories => categories.id < 200).map(
-              category => (
-                <option value="text">{category.name}</option>
-              )
-            )
-          }
-        </select>
-        <h3>Teatr</h3>
-        <select name="prferences">
-          {
-            this.state.categories.filter(categories => categories.id > 199).filter(categories => categories.id < 300).map(
-              category => (
-                <option value="text">{category.name}</option>
-              )
-            )
-          }
-        </select>
-        <h3>Muzyka</h3>
-        <select name="prferences">
-          {
-            this.state.categories.filter(categories => categories.id > 299).filter(categories => categories.id < 400).map(
-              category => (
-                <option value="text">{category.name}</option>
-              )
-            )
-          }
-        </select>
-        <h3>Sztuka</h3>
-        <select name="prferences">
-          {
-            this.state.categories.filter(categories => categories.id > 399).filter(categories => categories.id < 500).map(
-              category => (
-                <option value="text">{category.name}</option>
-              )
-            )
-          }
-        </select>
-        <h3>Literatura</h3>
-        <select name="prferences">
-          {
-            this.state.categories.filter(categories => categories.id > 499).filter(categories => categories.id < 600).map(
-              category => (
-                <option value="text">{category.name}</option>
-              )
-            )
-          }
-        </select>
-        <h3>Rozrywka</h3>
-        <select name="prferences">
-          {
-            this.state.categories.filter(categories => categories.id > 599).filter(categories => categories.id < 700).map(
-              category => (
-                <option value="text">{category.name}</option>
-              )
-            )
-          }
-        </select>
-        <h3>Rekreacja</h3>
-        <select name="prferences">
-          {
-            this.state.categories.filter(categories => categories.id > 699).filter(categories => categories.id < 800).map(
-              category => (
-                <option value="text">{category.name}</option>
-              )
-            )
-          }
-        </select>
-        <h3>Nauka</h3>
-        <select name="prferences">
-          {
-            this.state.categories.filter(categories => categories.id > 899).filter(categories => categories.id < 1000).map(
-              category => (
-                <option value="text">{category.name}</option>
-              )
-            )
-          }
-        </select>
-        <h3>Inne</h3>
-        <select name="prferences">
-          {
-            this.state.categories.filter(categories => categories.id > 999).filter(categories => categories.id < 1100).map(
-              category => (
-                <option value="text">{category.name}</option>
-              )
-            )
-          }
-        </select>
+        <form>
+          <div>
+            <label><input type="checkbox"/>Kino</label>
+            <select name="prferences">
+              {
+                this.state.categories.filter(categories => categories.id > 99).filter(categories => categories.id < 200).map(
+                  category => (
+                    <option value="text">{category.name}</option>
+                  )
+                )
+              }
+            </select>
+          </div>
+          <div>
+            <label><input type="checkbox"/>Teatr</label>
+            <select name="prferences">
+              {
+                this.state.categories.filter(categories => categories.id > 199).filter(categories => categories.id < 300).map(
+                  category => (
+                    <option value="text">{category.name}</option>
+                  )
+                )
+              }
+            </select>
+          </div>
+          <div>
+            <label><input type="checkbox"/>Muzyka</label>
+            <select name="prferences">
+              {
+                this.state.categories.filter(categories => categories.id > 299).filter(categories => categories.id < 400).map(
+                  category => (
+                    <option value="text">{category.name}</option>
+                  )
+                )
+              }
+            </select>
+          </div>
+          <div>
+            <label><input type="checkbox"/>Sztuka</label>
+            <select name="prferences">
+              {
+                this.state.categories.filter(categories => categories.id > 399).filter(categories => categories.id < 500).map(
+                  category => (
+                    <option value="text">{category.name}</option>
+                  )
+                )
+              }
+            </select>
+          </div>
+          <div>
+            <label><input type="checkbox"/>Literatura</label>
+            <select name="prferences">
+              {
+                this.state.categories.filter(categories => categories.id > 499).filter(categories => categories.id < 600).map(
+                  category => (
+                    <option value="text">{category.name}</option>
+                  )
+                )
+              }
+            </select>
+          </div>
+          <div>
+            <label><input type="checkbox"/>Rozrywka</label>
+            <select name="prferences">
+              {
+                this.state.categories.filter(categories => categories.id > 599).filter(categories => categories.id < 700).map(
+                  category => (
+                    <option value="text">{category.name}</option>
+                  )
+                )
+              }
+            </select>
+          </div>
+          <div>
+            <label><input type="checkbox"/>Rekreacja</label>
+            <select name="prferences">
+              {
+                this.state.categories.filter(categories => categories.id > 699).filter(categories => categories.id < 800).map(
+                  category => (
+                    <option value="text">{category.name}</option>
+                  )
+                )
+              }
+            </select>
+          </div>
+          <div>
+            <label><input type="checkbox"/>Nauka</label>
+            <select name="prferences">
+              {
+                this.state.categories.filter(categories => categories.id > 899).filter(categories => categories.id < 1000).map(
+                  category => (
+                    <option value="text">{category.name}</option>
+                  )
+                )
+              }
+            </select>
+          </div>
+          <div>
+            <label><input type="checkbox"/>Inne</label>
+            <select name="prferences">
+              {
+                this.state.categories.filter(categories => categories.id > 999).filter(categories => categories.id < 1100).map(
+                  category => (
+                    <option value="text">{category.name}</option>
+                  )
+                )
+              }
+            </select>
+          </div>
+        </form>
       </Fragment>
 
     )
