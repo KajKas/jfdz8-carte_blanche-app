9ed13ee4e86d03f5577603569ed801c919bdbfbc
Sebastian Kreft <sebastian.kreft@airhelp.com>
2018-10-02 21:02:11 +0200

    Fixed login form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './App.css';
 import EventsMap from "./EventsMap";
 import EventsList from "./EventsList";
@@ -11,25 +11,17 @@ import firebase from 'firebase';
 class App extends Component {
     state = {
         events: [],
-        email: '',
-        password: '',
         user: null,
     }
 
-    handleSubmit = event => {
+    createAccount = (event, email, password) => {
         event.preventDefault()
-        firebase.auth().createUserWithEmailAndPassword(
-            this.state.email,
-            this.state.password
-        )
+        firebase.auth().createUserWithEmailAndPassword(email, password)
     }
 
-    logIn = event => {
+    logIn = (event, email, password) => {
         event.preventDefault()
-        firebase.auth().signInWithEmailAndPassword(
-            this.state.email,
-            this.state.password
-        )
+        firebase.auth().signInWithEmailAndPassword(email, password)
     }
 
     signOut = (event) => {
@@ -37,98 +29,103 @@ class App extends Component {
         firebase.auth().signOut()
     }
 
-  componentDidMount() {
-      this.getEvents()
-      firebase.auth().onAuthStateChanged(
-          user => this.setState({ user })
-      )
-  }
-
-  getEvents() {
-    let eventsPromise = fetch('https://isa-cors-proxy.herokuapp.com/api/rest/events.json').then(response => { return response.json() });
-    let placesPromise = fetch('https://isa-cors-proxy.herokuapp.com/api/rest/places.json').then(response => { return response.json() });
-
-    placesPromise.then(
-      places => places.reduce(
-        (result, next) => {
-          result[next.id] = next
-          return result
-        }, {}
-      )
-    ).then(
-      placesObject => eventsPromise.then(
-        events => events.map(
-          event => ({
-            ...event,
-            place: placesObject[event.place.id]
-          })
+    componentDidMount() {
+        this.getEvents()
+        firebase.auth().onAuthStateChanged(
+            user => this.setState({user})
         )
-      )
-    ).then(
-      eventsWithPlaces => this.setState({ events: eventsWithPlaces })
-    )
-  }
+    }
 
-  render() {
-    return <Router>
-      <Fragment>
-        <div className="hero">
-          <div className="topbar">
-            <div className="topbar-menu">
-              <div>
-                <Link className="topbar-button topbar-button-1" to="/preferencesForm">Twój wybór</Link>
-              </div>
-              <div>
-                <Link className="topbar-button topbar-button-2" to="/eventsMap">Mapa wydarzeń</Link>
-              </div>
-              <div>
-                <Link className="topbar-button topbar-button-3" to="/eventsList">Lista wydarzeń</Link>
-              </div>
-            </div>
-            <div>
-              <button
-                onClick={this.signOut}
-                className="form-button logout-button"
-              >Wyloguj się
-              </button>
-            </div>
-          </div>
-            <Route path="/preferencesForm" component={PreferencesForm}/>
-            <Route
-              path="/eventsMap"
-              render={() => (
-                <EventsMap
-                  events={this.state.events}
-                />
-              )}
-            />
-            <Route
-              path="/eventsList"
-              render={() => (
-                <EventsList
-                  events={this.state.events}
-                />
-              )}
-            />
-            <Route
-              path="/events/:eventId"
-              render={
-                (props) => (
-                  <SingleEvent
-                    event={this.state.events.find(event => event.id === parseInt(props.match.params.eventId))}/>
+    getEvents() {
+        let eventsPromise = fetch('https://isa-cors-proxy.herokuapp.com/api/rest/events.json').then(response => {
+            return response.json()
+        });
+        let placesPromise = fetch('https://isa-cors-proxy.herokuapp.com/api/rest/places.json').then(response => {
+            return response.json()
+        });
+
+        placesPromise.then(
+            places => places.reduce(
+                (result, next) => {
+                    result[next.id] = next
+                    return result
+                }, {}
+            )
+        ).then(
+            placesObject => eventsPromise.then(
+                events => events.map(
+                    event => ({
+                        ...event,
+                        place: placesObject[event.place.id]
+                    })
                 )
-              }/>
-          <div className="sign-up-form">
-            <SignUpForm
-              handleSubmit={this.handleSubmit}
-              logIn={this.logIn}
-              user={this.state.user}
-            />
-          </div>
-        </div>
-      </Fragment>
-    </Router>
-  }
+            )
+        ).then(
+            eventsWithPlaces => this.setState({events: eventsWithPlaces})
+        )
+    }
+
+    render() {
+        return <Router>
+            <Fragment>
+                <div className="hero">
+                    <div className="topbar">
+                        <div className="topbar-menu">
+                            <div>
+                                <Link className="topbar-button topbar-button-1" to="/preferencesForm">Twój wybór</Link>
+                            </div>
+                            <div>
+                                <Link className="topbar-button topbar-button-2" to="/eventsMap">Mapa wydarzeń</Link>
+                            </div>
+                            <div>
+                                <Link className="topbar-button topbar-button-3" to="/eventsList">Lista wydarzeń</Link>
+                            </div>
+                        </div>
+                        <div>
+                            {this.state.user && <button
+                                onClick={this.signOut}
+                                className="form-button logout-button"
+                            >
+                                Wyloguj się
+                            </button>}
+                        </div>
+                    </div>
+                    <Route path="/preferencesForm" component={PreferencesForm}/>
+                    <Route
+                        path="/eventsMap"
+                        render={() => (
+                            <EventsMap
+                                events={this.state.events}
+                            />
+                        )}
+                    />
+                    <Route
+                        path="/eventsList"
+                        render={() => (
+                            <EventsList
+                                events={this.state.events}
+                            />
+                        )}
+                    />
+                    <Route
+                        path="/events/:eventId"
+                        render={
+                            (props) => (
+                                <SingleEvent
+                                    event={this.state.events.find(event => event.id === parseInt(props.match.params.eventId))}/>
+                            )
+                        }
+                    />
+
+                    {!this.state.user && <SignUpForm
+                        createAccount={this.createAccount}
+                        logIn={this.logIn}
+                    />}
+
+                </div>
+            </Fragment>
+        </Router>
+    }
 }
 
 export default App
