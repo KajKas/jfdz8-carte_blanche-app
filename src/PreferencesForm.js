5bd98f3524d30fe40343e00e0fcb4dd5f5d99170
ewa1siwek <ew.siwek@gmail.com>
2018-10-05 00:57:20 +0200

    Prefereces styled

--- src/PreferencesForm.js
+++ src/PreferencesForm.js
@@ -48,150 +48,226 @@ class PreferencesForm extends Component {
     return name
   }
 
-
   render() {
     return (
       <Fragment>
-        <div>
-        <form className="preferences-form">
-          <div className="preferences-item">
-            <label className="preferences-label">Wiek</label>
-            <select className="preferences-select" name="prferences">
-              <option className="preferences-option" value="text">{'<'}18</option>
-              <option className="preferences-option" value="text">{'18<21'}</option>
-              <option className="preferences-option" value="text">{'>21'}</option>
-            </select>
-          </div>
-          <div className="preferences-item">
-            <label className="preferences-label">Miasto</label>
-            <select className="preferences-select" name="preferences">
-              <option className="preferences-option" value="text">Gdańsk</option>
-              <option className="preferences-option" value="text">Sopot</option>
-              <option className="preferences-option" value="text">Gdynia</option>
-            </select>
-          </div>
-          <div className="preferences-item">
-            <label className="preferences-label">Kino</label>
-            <select className="preferences-select" name="preferences" onChange={this.handleSelect}>
-              {
-                this.state.categories.filter(categories => categories.id > 99).filter(categories => categories.id < 200).map(
+        <section className="preferences-form-wrapper">
+            <div className="preferences-form">
+              <div className="preferences-item">
+                <label className="preferences-label">
+                  <input className="preferences-input" type="checkbox"/>wiek
+                </label>
+                <select className="preferences-select" name="preferences">
+                  <option className="preferences-option" value="text">{'<18'}</option>
+                  <option className="preferences-option" value="text">{'18<21'}</option>
+                  <option className="preferences-option" value="text">{'>21'}</option>
+                </select>
+              </div>
+              <div className="preferences-item">
+                <label className="preferences-label">
+                  <input className="preferences-input" type="checkbox"/>miasto
+                </label>
+                <select className="preferences-select" name="preferences">
+                  <option className="preferences-option" value="text">Gdańsk</option>
+                  <option className="preferences-option" value="text">Sopot</option>
+                  <option className="preferences-option" value="text">Gdynia</option>
+                </select>
+              </div>
+              <div className="preferences-item">
+                <label className="preferences-label">
+                  <input className="preferences-input" type="checkbox"/>film
+                </label>
+                <select
+                  className="preferences-select"
+                  name="preferences"
+                  onChange={this.handleSelect}>{
+                    this.state.categories.filter(categories => categories.id > 99)
+                      .filter(categories => categories.id < 200)
+                      .map(category =>(
+                        <option
+                          className="preferences-option"
+                          value={category.id}>{category.name}
+                         </option>
+                      ))
+                  }
+                  </select>
+              </div>
+              <div className="preferences-item">
+                <label className="preferences-label">
+                   <input className="preferences-input" type="checkbox"/>teatr
+                </label>
+                <select
+                  className="preferences-select"
+                  name="preferences"
+                  onChange={this.handleSelect}>{
+                    this.state.categories.filter(categories => categories.id > 199)
+                      .filter(categories => categories.id < 300)
+                      .map(category => (
+                        <option
+                          className="preferences-option"
+                          value={category.id}>{category.name}
+                          </option>
+                      ))
+                    }
+                </select>
+              </div>
+              <div className="preferences-item">
+                <label className="preferences-label">
+                  <input className="preferences-input" type="checkbox"/>muzyka
+                </label>
+                <select
+                  className="preferences-select"
+                  name="preferences"
+                  onChange={this.handleSelect}>{
+                    this.state.categories.filter(categories => categories.id > 299)
+                      .filter(categories => categories.id < 400)
+                      .map(category => (
+                        <option
+                          className="preferences-option"
+                          value={category.id}>{category.name}
+                        </option>
+                       ))
+                    }
+                </select>
+              </div>
+              <div className="preferences-item">
+                <label className="preferences-label">
+                  <input className="preferences-input" type="checkbox"/>sztuka
+                </label>
+                <select
+                  className="preferences-select"
+                  name="preferences"
+                  onChange={this.handleSelect}>{
+                    this.state.categories.filter(categories => categories.id > 399)
+                      .filter(categories => categories.id < 500)
+                      .map(category => (
+                        <option className="preferences-option"
+                                value={category.id}>{category.name}
+                        </option>
+                      ))
+                  }
+                </select>
+              </div>
+              <div className="preferences-item">
+                <label className="preferences-label">
+                  <input className="preferences-input" type="checkbox"/>literatura
+                </label>
+                <select
+                  className="preferences-select"
+                  name="preferences"
+                  onChange={this.handleSelect}>{
+                    this.state.categories.filter(categories => categories.id > 499)
+                      .filter(categories => categories.id < 600)
+                      .map(category => (
+                        <option
+                          className="preferences-option"
+                          value={category.id}>{category.name}
+                        </option>
+                      ))
+                  }
+                </select>
+              </div>
+              <div className="preferences-item">
+                <label className="preferences-label">
+                  <input className="preferences-input" type="checkbox"/>rozrywka
+                </label>
+                <select
+                  className="preferences-select"
+                  name="preferences"
+                  onChange={this.handleSelect}>{
+                    this.state.categories.filter(categories => categories.id > 599)
+                      .filter(categories => categories.id < 700)
+                      .map(category => (
+                        <option
+                          className="preferences-option"
+                          value={category.id}>{category.name}
+                        </option>
+                      ))
+                  }
+                </select>
+              </div>
+              <div className="preferences-item">
+                <label className="preferences-label">
+                  <input className="preferences-input" type="checkbox"/>rekreacja
+                </label>
+                <select
+                  className="preferences-select"
+                  name="preferences"
+                  onChange={this.handleSelect}>{
+                    this.state.categories.filter(categories => categories.id > 699)
+                      .filter(categories => categories.id < 800)
+                      .map(category => (
+                        <option
+                          className="preferences-option"
+                          value={category.id}>{category.name}
+                        </option>
+                      ))
+                  }
+                </select>
+              </div>
+              <div className="preferences-item">
+                <label className="preferences-label">
+                  <input className="preferences-input" type="checkbox"/>nauka
+                </label>
+                <select
+                  className="preferences-select"
+                  name="preferences"
+                  onChange={this.handleSelect}>{
+                    this.state.categories.filter(categories => categories.id > 899)
+                      .filter(categories => categories.id < 1000)
+                      .map(category => (
+                        <option
+                          className="preferences-option"
+                          value={category.id}>{category.name}
+                        </option>
+                      ))
+                  }
+                </select>
+              </div>
+              <div className="preferences-item">
+                <label className="preferences-label">
+                  <input className="preferences-input" type="checkbox"/>inne
+                </label>
+                <select
+                  className="preferences-select"
+                  name="preferences"
+                  onChange={this.handleSelect}>{
+                    this.state.categories.filter(categories => categories.id > 999)
+                      .filter(categories => categories.id < 1100)
+                      .map(
                   category => (
-                    <option className="preferences-option" value={category.id}>{category.name}</option>
-                  )
-                )
-              }
-            </select>
-          </div>
-          <div className="preferences-item">
-            <label className="preferences-label">Teatr</label>
-            <select className="preferences-select" name="preferences" onChange={this.handleSelect}>
-              {
-                this.state.categories.filter(categories => categories.id > 199).filter(categories => categories.id < 300).map(
-                  category => (
-                    <option className="preferences-option" value={category.id}>{category.name}</option>
-                  )
-                )
-              }
-            </select>
-          </div>
-          <div className="preferences-item">
-            <label className="preferences-label">Muzyka</label>
-            <select className="preferences-select" name="preferences" onChange={this.handleSelect}>
-              {
-                this.state.categories.filter(categories => categories.id > 299).filter(categories => categories.id < 400).map(
-                  category => (
-                    <option className="preferences-option" value={category.id}>{category.name}</option>
-                  )
-                )
-              }
-            </select>
-          </div>
-          <div className="preferences-item">
-            <label className="preferences-label">Sztuka</label>
-            <select className="preferences-select" name="preferences" onChange={this.handleSelect}>
-              {
-                this.state.categories.filter(categories => categories.id > 399).filter(categories => categories.id < 500).map(
-                  category => (
-                    <option className="preferences-option" value={category.id}>{category.name}</option>
-                  )
-                )
-              }
-            </select>
-          </div>
-          <div className="preferences-item">
-            <label className="preferences-label">Literatura</label>
-            <select className="preferences-select" name="preferences" onChange={this.handleSelect}>
-              {
-                this.state.categories.filter(categories => categories.id > 499).filter(categories => categories.id < 600).map(
-                  category => (
-                    <option className="preferences-option" value={category.id}>{category.name}</option>
-                  )
-                )
-              }
-            </select>
-          </div>
-          <div className="preferences-item">
-            <label className="preferences-label">Rozrywka</label>
-            <select className="preferences-select" name="preferences" onChange={this.handleSelect}>
-              {
-                this.state.categories.filter(categories => categories.id > 599).filter(categories => categories.id < 700).map(
-                  category => (
-                    <option className="preferences-option" value={category.id}>{category.name}</option>
-                  )
-                )
-              }
-            </select>
-          </div>
-          <div className="preferences-item">
-            <label className="preferences-label">Rekreacja</label>
-            <select className="preferences-select" name="preferences" onChange={this.handleSelect}>
-              {
-                this.state.categories.filter(categories => categories.id > 699).filter(categories => categories.id < 800).map(
-                  category => (
-                    <option className="preferences-option" value={category.id}>{category.name}</option>
-                  )
-                )
-              }
-            </select>
-          </div>
-          <div className="preferences-item">
-            <label className="preferences-label">Nauka</label>
-            <select className="preferences-select" name="preferences" onChange={this.handleSelect}>
-              {
-                this.state.categories.filter(categories => categories.id > 899).filter(categories => categories.id < 1000).map(
-                  category => (
-                    <option className="preferences-option" value={category.id}>{category.name}</option>
-                  )
-                )
-              }
-            </select>
-          </div>
-          <div className="preferences-item">
-            <label className="preferences-label">Inne</label>
-            <select className="preferences-select" name="preferences" onChange={this.handleSelect}>
-              {
-                this.state.categories.filter(categories => categories.id > 999).filter(categories => categories.id < 1100).map(
-                  category => (
-                    <option className="preferences-option" value={category.id}>{category.name}</option>
-                  )
-                )
-              }
-            </select>
-          </div>
-          <div className="preferences-item">
-            <label>Cena od:</label>
-            <input type="range" min="0" max="1000" onChange={(event) => this.handleRangeFrom(event)}/>
-            <span>{this.state.rangeFrom}</span>
-          </div>
-          <div className="preferences-item">
-            <label>Cena do:</label>
-            <input type="range" min={this.state.rangeFrom} max="1000" onChange={(event) => this.handleRangeTo(event)}/>
-            <span>{this.state.rangeTo}</span>
-          </div>
-        </form>
-        </div>
-        <div className="chosen-categories">
-          <h3>Wybrane kategorie:</h3>
+                    <option
+                      className="preferences-option"
+                      value={category.id}>{category.name}
+                    </option>
+                  ))
+                }
+                </select>
+              </div>
+              <div className="preferences-price">
+                {/*<div>
+                  <label className="preferences-label">Cena od
+                    <span className="preferences-span">{this.state.rangeFrom + ' zł'}</span>
+                  </label>
+                  <input
+                    type="range"
+                    min="0" max="1000"
+                    onChange={(event) => this.handleRangeFrom(event)}
+                  />
+                </div>*/}
+                <div className="">
+                  <label className="preferences-label">cena do
+                    <span className="preferences-span">{" " + this.state.rangeTo + ' zł'}</span>
+                  </label>
+                  <input
+                    type="range"
+                    min={this.state.rangeFrom} max="1000"
+                    onChange={(event) => this.handleRangeTo(event)}
+                  />
+                </div>
+              </div>
+            <div className="preferences-chosen-categories">
+              <h3>Wybrane kategorie:</h3>
           {/*{*/}
             {/*this.props.activeCategories.map(*/}
               {/*activeCategory =>*/}
@@ -200,31 +276,23 @@ class PreferencesForm extends Component {
                 {/*</button>*/}
             {/*)*/}
           {/*}*/}
-
-
-        {
-          this.props.activeCategories.map(
-            activeCategory =>
-              <button onClick={() => this.props.deleteActiveCategory(parseInt(activeCategory))}>
-
-
-
-                {
-                  this.state.categories.length ?
-                  this.debug(activeCategory) :
-                    null
-
-
+          {
+                this.props.activeCategories.map(
+                  activeCategory =>
+                    <button
+                      className="preferences-select preference-button"
+                      onClick={() => this.props.deleteActiveCategory(parseInt(activeCategory))}>{
+                      this.state.categories.length ? this.debug(activeCategory) : null
                   // this.state.categories ?
                   // setTimeout(function () {
                   //
                   // }, 1000) : null
-
-                }
-              </button>
-          )
-        }
-        </div>
+                      }
+                    </button>
+                )}
+             </div>
+            </div>
+        </section>
       </Fragment>
 
     )
